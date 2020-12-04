9ddf1dc4f2f7086e2713295a12d59bc4bc235f8d
Joey Gibson <joey@joeygibson.com>
2020-12-03 23:35:29 -0500

    day 3, part 2

--- day3/day3.rb
+++ day3/day3.rb
@@ -2,23 +2,36 @@
 
 the_map = File.readlines('input.txt').map(&:strip)
 
-off_the_map_right = the_map[0].length - 1
-off_the_map_bottom = the_map.length - 1
+def count_trees(the_map, move_right, move_down)
+  off_the_map_right = the_map[0].length - 1
+  off_the_map_bottom = the_map.length - 1
 
-move_right = 3
-move_down = 1
+  col = 0
+  row = 0
+  trees = 0
 
-col = 0
-row = 0
-trees = 0
+  while row < off_the_map_bottom
+    col += move_right
+    row += move_down
 
-while row < off_the_map_bottom
-  col += move_right
-  row += move_down
+    col -= (off_the_map_right + 1) if col > off_the_map_right
 
-  col -= (off_the_map_right + 1) if col > off_the_map_right
+    trees += 1 if the_map[row][col] == '#'
+  end
 
-  trees += 1 if the_map[row][col] == '#'
+  puts "move_right = #{move_right}, move_down = #{move_down}, trees = #{trees}"
+  trees
 end
 
-puts "Trees: #{trees}"
+puts "Part 1 Trees: #{count_trees(the_map, 3, 1)}"
+
+pairs = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+total_trees = pairs.inject(1) do |total, pair|
+  total * count_trees(the_map, pair[0], pair[1])
+end
+
+puts "Part 2 Trees: #{total_trees}"
+
+
+
