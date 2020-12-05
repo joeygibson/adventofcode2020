339330fb62003c1b70a7df2d2df9f56084f8f6bb
Joey Gibson <joey@joeygibson.com>
2020-12-05 01:38:10 -0500

    day 5, part 1

--- day5/day5.rb
+++ day5/day5.rb
@@ -5,47 +5,34 @@ seats = Array(0..7)
 
 boarding_passes = File.readlines('input.txt').map(&:strip)
 
-boarding_passes.map do |boarding_pass|
-  sub_rows = rows
+def bisect(arr, boarding_pass)
+  sub = arr
 
-  selected_row = nil
-  selected_seat = nil
-
-  boarding_pass.split(//).take(7).each do |letter|
-    lo, hi = if letter == 'F'
-               [0, sub_rows.length / 2 - 1]
+  boarding_pass.each do |letter|
+    lo, hi = if %w[F L].include?(letter)
+               [0, sub.length / 2 - 1]
              else
-               [sub_rows.length / 2, sub_rows.length - 1]
+               [sub.length / 2, sub.length - 1]
              end
 
-    if lo == hi
-      selected_row = sub_rows[lo]
-    else
-      sub_rows = sub_rows[lo..hi]
-    end
-  end
+    return sub[lo] if lo == hi
 
-  sub_rows = seats
-  boarding_pass.split(//).drop(7).each do |letter|
-    lo, hi = if letter == 'L'
-               [0, sub_rows.length / 2 - 1]
-             else
-               [sub_rows.length / 2, sub_rows.length - 1]
-             end
-
-    if lo == hi
-      selected_seat = sub_rows[lo]
-    else
-      sub_rows = sub_rows[lo..hi]
-    end
-  end
-
-  seat_id = selected_row * 8 + selected_seat
-
-  if boarding_pass == 'BFFFBBFRRR' || boarding_pass == 'FFFBBBFRRR' || boarding_pass == 'BBFFBBFRLL'
-    puts "BP: #{boarding_pass}, row: #{selected_row}, seat: #{selected_seat} id: #{seat_id}"
+    sub = sub[lo..hi]
   end
 end
+
+seat_ids = boarding_passes.map do |boarding_pass|
+  selected_row = bisect(rows, boarding_pass.split(//).take(7))
+  selected_seat = bisect(seats, boarding_pass.split(//).drop(7))
+
+  selected_row * 8 + selected_seat
+
+  # puts "BP: #{boarding_pass}, row: #{selected_row}, seat: #{selected_seat} id: #{seat_id}"
+end
+
+highest_id = seat_ids.sort.reverse.first
+
+puts "highest_id: #{highest_id}"
 
 
 
