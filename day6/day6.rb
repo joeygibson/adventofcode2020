0e862adfea4313488fba6e3e9b1322b82b0d67dc
Joey Gibson <joey@joeygibson.com>
2020-12-06 00:42:08 -0500

    day 6, part 2

--- day6/day6.rb
+++ day6/day6.rb
@@ -14,12 +14,37 @@ def process_group_any(group)
   consolidated_answers.length
 end
 
-group_answers = File.read('input.txt').split(/\n\n/).map do |group|
+def process_group_all(group)
+  consolidated_answers = Hash.new(0)
+
+  group.split(/\n/).each do |answers|
+    answers.split(//) do |letter|
+      consolidated_answers[letter] += 1
+    end
+  end
+
+  consolidated_answers.select { |key, value|
+    value == group.split(/\n/).length
+  }.length
+end
+
+input = File.read('input.txt').split(/\n\n/)
+
+group_answers_any = input.map do |group|
   process_group_any(group)
 end
 
-total = group_answers.inject(0) do |sum, answers|
+group_answers_all = input.map do |group|
+  process_group_all(group)
+end
+
+total_any = group_answers_any.inject(0) do |sum, answers|
   sum + answers
 end
 
-puts "Total: #{total}"
+total_all = group_answers_all.inject(0) do |sum, answers|
+  sum + answers
+end
+
+puts "Total (any): #{total_any}"
+puts "Total (all): #{total_all}"
