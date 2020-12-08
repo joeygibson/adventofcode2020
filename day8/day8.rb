3bbcff4578c69e99bdecc021c42c978f3a529f25
Joey Gibson <joey@joeygibson.com>
2020-12-08 00:59:19 -0500

    day 8, part 2

--- day8/day8.rb
+++ day8/day8.rb
@@ -8,12 +8,19 @@ instruction_cache = Set.new
 
 acc = 0
 ip = 0
+old_ip = 0
+nop_cache = []
+jmp_cache = []
 
 loop do
+  break if ip >= program.length
+
   line = program[ip]
 
   if instruction_cache.include?(ip)
-    puts "infinite loop! acc = #{acc}"
+    puts "infinite loop at instruction! acc = #{acc}"
+    puts "\tnop_cache: #{nop_cache.reverse.take(10)}"
+    puts "\tjmp_cache: #{jmp_cache.reverse.take(10)}"
     break
   else
     instruction_cache << ip
@@ -23,6 +30,7 @@ loop do
   case op
   when 'nop'
     # do nothing
+    nop_cache << ip
   when 'acc'
     if sign == '+'
       acc += arg.to_i
@@ -30,6 +38,9 @@ loop do
       acc -= arg.to_i
     end
   when 'jmp'
+    jmp_cache << ip
+    old_ip = ip
+
     if sign == '+'
       ip += arg.to_i
     else
@@ -40,5 +51,8 @@ loop do
     puts "invalid op: #{op}"
   end
 
+  old_ip = ip
   ip += 1
 end
+
+puts "program terminated; acc = #{acc}"
