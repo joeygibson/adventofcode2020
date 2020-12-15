9bf1d979bf75dc2be0a5399bdd40eb915e95ff8f
Joey Gibson <joey@joeygibson.com>
2020-12-15 01:12:15 -0500

    day 15, part 2

--- day15/day15.rb
+++ day15/day15.rb
@@ -1,29 +1,39 @@
 #!/usr/bin/env ruby
 
-require 'matrix'
-
-if ARGV.length != 1
-  puts 'Usage: day15.rb <input file>'
+if ARGV.length != 2
+  puts 'Usage: day15.rb <input file> <iterations>'
   exit(1)
 end
 
-spoken_numbers = []
+iterations = ARGV[1].to_i
+spoken_numbers = {}
+last_spoken = nil
 
 starting_numbers = File.read(ARGV[0]).split(',').reject(&:empty?).map(&:strip).map(&:to_i)
 
-spoken_numbers += starting_numbers
-
-while spoken_numbers.length < 2020
-  last_spoken = spoken_numbers.last
-
-  last_time_spoken = spoken_numbers.take(spoken_numbers.length - 1).rindex { |n| n == last_spoken }
-
-  if last_time_spoken.nil? # never spoken before
-    spoken_numbers << 0
-  else
-    new_index = spoken_numbers.length - 1 - last_time_spoken
-    spoken_numbers << new_index
-  end
+starting_numbers.each_with_index do |num, idx|
+  bucket = spoken_numbers[num.to_s] || []
+  bucket << idx + 1
+  spoken_numbers[num] = bucket
+  last_spoken = num
 end
 
-puts "2020th spoken number: #{spoken_numbers.last}"
+starting_length = starting_numbers.length + 1
+
+(starting_length..iterations).each do |turn|
+  last_time_spoken = spoken_numbers[last_spoken]
+
+  last_spoken = if last_time_spoken.empty? ||
+    (last_time_spoken.length == 1 && last_time_spoken.last == turn - 1) # never spoken before
+                  0
+                else
+                  count = last_time_spoken.length
+                  last_time_spoken[count - 1] - last_time_spoken[count - 2]
+                end
+
+  bucket = spoken_numbers[last_spoken] || []
+  bucket << turn
+  spoken_numbers[last_spoken] = bucket
+end
+
+puts "#{iterations}th spoken number: #{last_spoken}"
