d2e7685df2f345f0188d17574c9fb97b178a82c7
Joey Gibson <joey@joeygibson.com>
2020-12-17 07:46:10 -0500

    day 17, part 2

--- day17/day17.rb
+++ day17/day17.rb
@@ -14,26 +14,30 @@ board = Hash.new { false }
 (-1..dim).each do |c_idx|
   (-1..dim).each do |r_idx|
     (-1..1).each do |z_idx|
-      coord = [c_idx, r_idx, z_idx]
-      board[coord] = false
+      (-1..1).each do |w_idx|
+        coord = [c_idx, r_idx, z_idx, w_idx]
+        board[coord] = false
+      end
     end
   end
 end
 
 input.each_with_index do |row, r_idx|
   row.each_with_index do |state, c_idx|
-    board[[c_idx, r_idx, 0]] = state == '#'
+    board[[c_idx, r_idx, 0, 0]] = state == '#'
   end
 end
 
-def get_neighbors(board, col, row, layer)
+def get_neighbors(board, col, row, layer, w)
   neighbors = {}
 
   (col - 1..col + 1).each do |c_idx|
     (row - 1..row + 1).each do |r_idx|
       (layer - 1..layer + 1).each do |z_idx|
-        new_coord = [c_idx, r_idx, z_idx]
-        neighbors[new_coord] = board[new_coord] || false if new_coord != [col, row, layer]
+        (w - 1..w + 1).each do |w_idx|
+          new_coord = [c_idx, r_idx, z_idx, w_idx]
+          neighbors[new_coord] = board[new_coord] || false if new_coord != [col, row, layer, w]
+        end
       end
     end
   end
@@ -45,8 +49,8 @@ def turn(board)
   new_board = {}
 
   board.each do |coords, active|
-    col, row, layer = coords
-    neighbors = get_neighbors(board, col, row, layer)
+    col, row, layer, w = coords
+    neighbors = get_neighbors(board, col, row, layer, w)
     active_neighbors = neighbors.values.count do |state|
       state
     end
