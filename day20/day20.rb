d326325c80a8af18d2c177c6a8e71b705d5ddeb0
Joey Gibson <joey@joeygibson.com>
2020-12-20 01:54:39 -0500

    day 20, part 1 - works

--- day20/day20.rb
+++ day20/day20.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 if ARGV.length != 1
   puts "Usage: #{__FILE__} <input file>"
   exit(1)
@@ -25,25 +27,49 @@ def get_column(tile, col_idx)
   col.join('')
 end
 
+unique_edges = Hash.new {0}
+  #Set.new
+
 raw_edges = tiles.flat_map do |tile_id, tile|
   edges = {}
 
   edges[:top] = tile[0]
+  edges[:top_r] = tile[0].reverse
   edges[:bottom] = tile[-1]
+  edges[:bottom_r] = tile[-1].reverse
   edges[:left] = get_column(tile, 0)
+  edges[:left_r] = get_column(tile, 0).reverse
   edges[:right] = get_column(tile, -1)
+  edges[:right_r] = get_column(tile, -1).reverse
+
+  edges.each_value do |edge|
+    unique_edges[edge] += 1
+  end
 
   [tile_id, edges]
 end
 
 edges = Hash[raw_edges.each_slice(2).to_a]
 
-edges.each do |tile_id, tile_edges|
-  edges.reject {|id| id == tile_id}.each do |tile_id1, tile_edges1|
-    tile_edges.each do |dir, edge|
-      tile_edges1.each do |dir1, edge1|
-        puts "match: #{tile_id} #{dir},\t#{tile_id1}, #{dir1},\t#{edge}" if edge == edge1
+ue1 = unique_edges.reject {|edge, count| count > 1}
+
+matches = Hash.new {0}
+
+ue1.each_key do |edge|
+  edges.each do |tile_id, tile_edges|
+    tile_edges.each_value do |tile_edge|
+      if tile_edge == edge
+        # puts "#{tile_id}" #, #{edge}"
+        matches[tile_id] += 1
       end
     end
   end
 end
+
+m = matches.select do |k, v|
+  v == 4
+end
+
+res = m.keys.map(&:to_i).inject(1) {|acc, e| acc * e}
+
+puts "#{res}"
